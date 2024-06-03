5711bcee85ed457e67cdacfe1dadf5302a44e723
Antonio Peña <angeles_yanco@hotmail.com>
2024-06-03 13:27:30 -0600

    Module 4 Logic. Class 1: creation of a publication.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,18 +8,13 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function dashboard(Request $request){
-        //dd($request->all());
-        //*dd($request->get('for-my'));
-        //dd($request->user()->id);
 
         if($request->get('for-my')){
-            //$posts = Post::where('user_id', $request->user()->id)->latest()->get();
-            $posts = $request->user()->posts;
+            $posts = $request->user()->posts()->latest()->get();
         }else{
             $posts = Post::latest()->get();
         }
 
-        //return view('dashboard', ['posts' => $posts]);
         return view('dashboard', compact('posts'));
     }
 }
