9271779c274ab3968f60ba6f8c82baff4b1f2678
Antonio Peña <angeles_yanco@hotmail.com>
2024-06-12 19:34:30 -0600

    Module 5 review, class 1: optimization.

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -10,26 +10,24 @@ class FriendController extends Controller
     //
     public function store(Request $request, User $user){
 
-        $is_from = $request->user()->from()->where('to_id', $user->id)->exists();
-        $is_to = $request->user()->from()->where('from_id', $user->id)->exists();
+        // $is_from = $request->user()->from()->where('to_id', $user->id)->exists();
+        // $is_to = $request->user()->from()->where('from_id', $user->id)->exists();
 
-        if($is_from || $is_to){
-            return back();
+        if(!$request->user()->isRelated($user)){
+            $request->user()->from()->attach($user);
         }
 
-        if($request->user()->id === $user->id){
-            return back();
-        }
+        // if($request->user()->id === $user->id){
+        //     return back();
+        // }
 
-        $request->user()->from()->attach($user);
+        //$request->user()->from()->attach($user);
 
         return back();
     }
 
     public function update(Request $request, User $user){
         
-        //dd("Usuario: ".$user->id,"Solicitud: ".$request->user()->pendingTo);
-        //$request->user()->pendingTo()->where('from_id', $user->id)->update(['accepted' => true]);
         $request->user()->pendingTo()->updateExistingPivot($user, ['accepted' => true]);
 
         return back();
